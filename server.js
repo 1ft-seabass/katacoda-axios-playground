d7f1c49ccd16b6a692fe25ecab969d1f4584891c
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-08-23 00:15:18 +0900

    upload

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 
 // Get HTTPbin GET Request
 // It is copy response call.
-axios.get('https://httpbin.org/')
+axios.get('https://httpbin.org/get')
   .then(function (response) {
     // handle success
-    console.log(response);
+    console.log(response.data);
   })
   .catch(function (error) {
     // handle error
